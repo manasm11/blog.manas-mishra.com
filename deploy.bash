68e1d41aa56e384e365daeb69250ed3ee2d9d8a8
Manas Mishra <manas.m22@gmail.com>
2023-11-16 23:03:09 +0530

    Updated deploy.bash file

--- deploy.bash
+++ deploy.bash
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-git status && git add . && git commit && git push && rm -rf public/ resources/ && hugo && firebase deploy
+git status
+echo "Do you wish to deploy the changes?"
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes ) git add . && git commit && git push && rm -rf public/ resources/ && hugo && firebase deploy;;
+    No ) exit;;
+  esac
+done
+
